4de547a3608c1182204713c97fc43899ee1c7b62
Azizbek Tosh <Azizbek>
2021-11-24 18:23:09 +0500

    cart logic fixed

--- birzum/static/js/project.js
+++ birzum/static/js/project.js
@@ -3,14 +3,15 @@
 $(document).ready(function() {
     $('.btn-cart').on('click', function(e){
         e.preventDefault();
-        console.log("fucking shit")
+        console.log("fucking shit is gets executed")
         $.ajax({
             method: "GET",
             url: $(this).attr("href"),
             dataType: 'json',
             success: function (data) {
+                console.log("ajax is completed")
                 if (data) {
-                   console.log(data.success) 
+                   console.log(data.success)
                 }
             }
         });
