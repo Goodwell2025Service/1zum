94082798ddc8c6c16d52e13116b30a5e7e9b3f63
Azizbek Tosh <Azizbek>
2021-12-06 16:30:55 +0500

    thousand sep

--- birzum/homeviews.py
+++ birzum/homeviews.py
@@ -1,6 +1,6 @@
 from django.views.generic import TemplateView
+from birzum.apps.products.models import Category, Product
 
-# from birzum.apps.products.models import Category, Product
 class HomeView(TemplateView):
     template_name = "pages/home.html"
 
@@ -11,12 +11,12 @@ class HomeView(TemplateView):
         ctx['day_hot_sale'] = 'something'
         ctx['week_hot_sale'] = 'something'
         ctx['hot_categories'] = 'something'
-        ctx['hot_electronics'] = 'something'
-        ctx['hot_clothes'] = 'something'
-        ctx['bestsellers'] = 'something'
+        ctx['hot_electronics'] = Product.objects.filter(category=2)[:8]
+        ctx['hot_clothes'] = Product.objects.filter(category=3)[:8]
         ctx['top_rated'] = 'something'
         ctx['in_sale'] = 'something'
-        ctx['latest'] = 'something'
+        ctx['bestsellers'] = Product.objects.filter(bestseller=True, available=True)
+        ctx['latest'] = Product.objects.all().order_by('-created')[:3]
         ctx['partners'] = 'something'
         ctx['latest_news'] = 'something'
         ctx['recently_viewed'] = 'something'
