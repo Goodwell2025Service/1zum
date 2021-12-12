e2f8c4004075aa82f4fc24bbeb8a9b7bb07c4446
UIbrohimov <ubuhobbit@gmail.com>
2021-12-12 16:52:40 +0500

    fixing home slider, justify content center caused breaking all pages styling

--- birzum/apps/products/templatetags/custom_tags.py
+++ birzum/apps/products/templatetags/custom_tags.py
@@ -6,3 +6,12 @@ register = template.Library()
 @register.filter(name='addstr')
 def add_str(s1, s2):
     return str(s1) + str(s2)
+
+@register.inclusion_tag("templatetags/banner_info.html")
+def get_banner_info(info):
+    
+    return {
+        "red": info.split(" ")[0],
+        "black": " ".join(info.split(" ")[1:]), 
+    }
+
