8c7ba85c9a724ed7fd709214aea7396abc3df36e
ubuhobbit <ubuhobbit@gmail.com>
2022-01-23 17:24:59 +0500

    Implemented displaying ad banners

--- birzum/apps/blog/views.py
+++ birzum/apps/blog/views.py
@@ -9,9 +9,21 @@ class BlogListView(ListView):
     def get_queryset(self):
         qs =  super().get_queryset()
         q = self.request.GET.get('q', None)
+        discount = self.request.GET.get('discount', None)
+        new = self.request.GET.get('new', None)
         if q:
-            return qs.filter(title__icontains=q)
+            qs = qs.filter(title__icontains=q)
+        if discount:
+            qs = qs.filter(stock=True)
+        if new:
+            qs = qs.filter(news=True)
         return qs
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["news"] = New.objects.all().order_by('-created')[:6]
+        return context
+    
 
 
 list_view = BlogListView.as_view()
--- birzum/homeviews.py
+++ birzum/homeviews.py
@@ -2,7 +2,7 @@ from django.views.generic import TemplateView
 
 from birzum.apps.products.models import Product
 from birzum.apps.products.last_seen import Last
-from birzum.apps.smallapps.reklama.models import HomePageBanner
+from birzum.apps.smallapps.reklama.models import *
 from birzum.apps.smallapps.company.models import Features
 from birzum.apps.blog.models import Partner, New
 # from birzum.apps.products.models import Category, Product
@@ -15,10 +15,14 @@ class HomeView(TemplateView):
         ctx['last_seen'] = Product.objects.filter(
             id__in=last.box.keys()).select_related('brand', 'category').prefetch_related('image')
         ctx['banners'] = HomePageBanner.objects.all()
+        ctx['big_vertical'] = HorizontalAdvert.objects.last()
+        ctx['half_page'] = HalfPageAdvert.objects.all()[:2]
+        ctx['right_side'] = SidebarAdvert.objects.last()
+        ctx['top_prods'] = TopProductsAdvert.objects.last()
         ctx['features'] = Features.objects.all()
         ctx['partners'] = Partner.objects.all()
         ctx['products'] = Product.objects.select_related(
-            'brand', 'category').prefetch_related('image')[:3]
+            'brand', 'category').prefetch_related('image')[:8]
         ctx['bestseller'] = Product.objects.filter(
             bestseller=True).select_related('brand', 'category').prefetch_related('image')[:3]
         ctx['posts'] = New.objects.all().order_by('created')
