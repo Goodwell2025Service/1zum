7d0dcab4b9d44eedcbf32e7bcdf6eb53f9581136
AbdumalikJavaDeveloper265 <94728016+AbdumalikJavaDeveloper265@users.noreply.github.com>
2022-02-13 13:58:14 +0500

    Menyuni alohida htmlga olish va barcha so'zlarni o'zbekchada trans tegiga solib chiqish

--- birzum/users/urls.py
+++ birzum/users/urls.py
@@ -8,7 +8,7 @@ app_name = "users"
 urlpatterns = [
     path("~redirect/", view=user_redirect_view, name="redirect"),
     path("~update/", view=user_update_view, name="update"),
-    path("<str:username>/", view=user_account_dashboard_view, name="detail"),
+    path("<str:username>/detail/", view=user_account_dashboard_view, name="detail"),
     path("<str:username>/account_details/", view=user_update_view, name="account_details"),
     path("<str:username>/account_orders/", view=user_account_orders_view, name="account_orders"),
     path("<str:username>/account_addresses/", view = user_account_addresses_view, name="account_addresses"),
